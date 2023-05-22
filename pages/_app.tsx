081d45c7bccbeb38b416f1cdcb9985bc5c5a7c86
mikeedwards83 <mike@glass.lu>
2023-05-22 19:25:26 +0100

    fixing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Flowbite } from 'flowbite-react'
+import { DarkThemeToggle, Flowbite } from 'flowbite-react'
 
 function MyApp({ Component, pageProps }: AppProps) {
 
